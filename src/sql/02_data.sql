0a9404b9620e3d926e4105728619e165e63ae5e3
Dmitry Erokhin <dmitry.erokhin@gmail.com>
2017-10-28 22:57:22 +0200

    zq-7: Followup fixes

--- src/sql/02_data.sql
+++ src/sql/02_data.sql
@@ -1,10 +1,3 @@
-CREATE OR REPLACE FUNCTION zq.open_batch(p_queue_name TEXT, p_max_batch_size INT) RETURNS INT AS
-$$
-BEGIN
-  RAISE EXCEPTION 'Not implemented yet';
-END;
-$$ LANGUAGE plpgsql;
-
 CREATE OR REPLACE FUNCTION zq.enqueue(p_queue_name TEXT, p_data TEXT[]) RETURNS VOID AS
 $$
 BEGIN
